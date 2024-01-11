b2d5d2bb90c9b15d5a7472b2b6a47279501675ae
pfeiff190100 <pfeiff190100@sr.htlweiz.at>
2024-01-11 11:21:43 +0100

    add visualizer

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import csv
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 class DataVisualizer:
     def __init__(self, file):
         self.file = file
+
     def read_data(self):
         data = []
         with open(self.file, 'r') as f:
@@ -12,6 +14,19 @@ class DataVisualizer:
                 data.append(row)
         return data
 
+    def visualize_data(self, data, testmode=False):
+        dates = [datetime.fromtimestamp(int(row[1])) for row in data]
+        prices = [float(row[2]) for row in data]
+
+        if not testmode:
+            plt.plot(dates, prices)
+            plt.xlabel('Date')
+            plt.ylabel('Price')
+            plt.show()
+        else:
+            return dates, prices
+
 if __name__ == "__main__":
     visualizer = DataVisualizer("data/test_data.csv")
     data = visualizer.read_data()
+    visualizer.visualize_data(data)
